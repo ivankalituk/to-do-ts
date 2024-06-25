62339a7a1b43740f5173dae170ae714285b009b4
Ivan Kalitiul <ivankalytuk23@gmail.com>
2024-06-25 22:55:10 +0200

    Start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
-// компоненты
-import Header from '@/components/header/header';
-
-// страницы
-import MainPage from '@/pages/mainPage';
-import TaskPage from '@/pages/taskPage';
-
 
 function App() {
   return (
     <div className="App">
-      <Header />
 
       <main>
-        <Routes>
-          <Route Component={MainPage} path='/'/>
-          <Route Component={TaskPage} path='/TaskPage/:task_id'/>
-        </Routes>
+        gay
       </main>
     </div>
   );
